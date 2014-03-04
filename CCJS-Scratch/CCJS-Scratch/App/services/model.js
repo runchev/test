3b67acc91bcd5b6c617d905f61e8179217594b30
runchev <b_runcev@hotmail.com>
2014-03-04 10:57:10 +0100

    CCJS-Scratch added

---
+++ CCJS-Scratch/CCJS-Scratch/App/services/model.js
@@ -0,0 +1,91 @@
+﻿define(['config'], function (config) {
+    var imageSettings = config.imageSettings;
+    var nulloDate = new Date(1990, 0, 1);
+
+    var orderBy = {
+        speaker: 'firstName, lastName',
+        session: 'timeSlotId, level, speaker.firstName'
+    };
+
+    var entityNames = {
+        speaker: 'Person',
+        session: 'Session',
+        room: 'Room',
+        track: 'Track',
+        timeslot: 'TimeSlot'
+    };
+    var createNullos = function(manager) {
+        var unchanged = breeze.EntityState.Unchanged;
+        createNullo(entityNames.timeslot, { start: nulloDate, isSessionSlot: true });
+        createNullo(entityNames.room);
+        createNullo(entityNames.track);
+        createNullo(entityNames.speaker, {firstName:'[Select a person]' });
+
+        function createNullo(entityName, values) {
+            var initialValues = values ||
+            { name: '[Select a ' + entityName.toLowerCase() + ' ]' };
+            return manager.createEntity(entityName,initialValues,unchanged);
+        }
+
+    };
+
+    var model = {
+        configureMetadataStore: configureMetadataStore,
+        entityNames: entityNames,
+        orderBy: orderBy,
+        createNullos: createNullos
+
+    };
+
+    return model;
+
+    //#region Internal Methods
+    function configureMetadataStore(metadataStore) {
+        metadataStore.registerEntityTypeCtor(
+            'Session', function () { this.isPartial = false; }, sessionInitializer);
+        metadataStore.registerEntityTypeCtor(
+            'Person', function () { this.isPartial = false; }, personInitializer);
+        metadataStore.registerEntityTypeCtor(
+            'TimeSlot', null, timeSlotInitializer);
+    }
+
+    function sessionInitializer(session) {
+        session.tagsFormatted = ko.computed({
+            read: function () {
+                var text = session.tags();
+                return text ? text.replace(/\|/g, ', ') : text;
+            },
+            write: function (value) {
+                session.tags(value.replace(/\, /g, '|'));
+            }
+        });
+    }
+
+    function personInitializer(person) {
+        person.fullName = ko.computed(function () {
+            var fn = person.firstName();
+            var ln = person.lastName();
+            return ln ? fn + ' ' + ln : fn;
+        });
+        person.imageName = ko.computed(function () {
+            return makeImageName(person.imageSource());
+        });
+    };
+    
+    function timeSlotInitializer(timeSlot) {
+        timeSlot.name = ko.computed(function () {
+            var start = timeSlot.start();
+            var value =((start-nulloDate===0))?
+                '[Select a timeslot]':
+                (start && moment.utc(start).isValid()) ?
+                    moment.utc(start).format('ddd hh:mm a') : '[Unknown]';
+            return value;
+        });
+    }
+    
+    function makeImageName(source) {
+        return imageSettings.imageBasePath +
+            (source || imageSettings.unknownPersonImageSource);
+    }
+    //#endregion
+});
---
+++ CCJS-Scratch/CCJS-Scratch/App/viewmodels/sessiondetail.js
@@ -0,0 +1,110 @@
+﻿define(['services/datacontext', 'durandal/plugins/router','durandal/app','services/logger','durandal/system'],
+    function (datacontext, router,app,logger,system) {
+        var session = ko.observable();
+        var rooms = ko.observableArray([]);
+        var tracks = ko.observableArray([]);
+        var timeSlots = ko.observableArray([]);
+        var isSaving = ko.observable(false);
+        var isDeleting = ko.observable(false);
+
+
+        var activate = function (routeData) {
+            var id = parseInt(routeData.id);
+            initlookups();
+            return datacontext.getSessionById(id, session);
+        };
+        var initlookups = function () {
+            rooms(datacontext.lookups.rooms);
+            tracks(datacontext.lookups.tracks);
+            timeSlots(datacontext.lookups.timeSlots);
+
+
+        };
+        var goBack = function () {
+            router.navigateBack();
+        };
+        var save = function () {
+            isSaving(true);
+            return datacontext.saveChanges().fin(complete);
+            
+            function complete(){
+                isSaving(false);
+            }
+        };
+        var cancel = function () {
+            datacontext.cancelChanges();
+        };
+        var hasChanges = ko.computed(function () {
+            return datacontext.hasChanges();
+        });
+        var canSave = ko.computed(function () {
+            return hasChanges() && !isSaving();
+        });
+        var canDeactivate = function() {
+            if (hasChanges()) {
+                var title = "Leave?";
+                var msg = "Really";
+                return app.showMessage(msg, title, ['Yes', 'No'])
+                    .then(confirm);
+
+                function confirm(selectedOptions) {
+                    if (selectedOptions === 'Yes') {
+                        cancel();
+                    }
+                    return selectedOptions;
+                }                
+            }
+            return true;
+        };
+        var deleteSession = function () {
+                var title = "Delete session?";
+                var msg = "Do you really want to delete this session?";
+                isDeleting(true);
+                return app.showMessage(msg, title, ['Yes', 'No'])
+                    .then(confirmDelete);
+
+                function confirmDelete(selectedItem) {
+                    if (selectedItem === 'Yes') {
+                        session().entityAspect.setDeleted();
+                        save()
+                        .then(success)
+                        .fail(failed)
+                        .fin(finish);
+  
+                        function success() {
+                            router.navigateTo('#session');
+                        }
+
+                        function failed(error) {
+                            cancel();
+                            var msgError ='Error'. error.message;
+                            logger.logError(msgError, error, system.getModuleId(vm), true);
+
+                        }
+
+                        function finish() {
+                            return selectedItem;
+                        }
+                    }
+                    isDeleting(false);
+                }
+        };
+        var vm = {
+            activate: activate,
+            goBack: goBack,
+            session: session,
+            rooms: rooms,
+            tracks: tracks,
+            timeSlots: timeSlots,
+            title: 'Session Details',
+            save: save,
+            cancel: cancel,
+            hasChanges: hasChanges,
+            canSave: canSave,
+            canDeactivate: canDeactivate,
+            deleteSession: deleteSession
+
+            
+        };
+        return vm;
+    });
